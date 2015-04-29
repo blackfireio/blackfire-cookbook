ee164d4aed8f81674663719fdfc78ae7acdbbc52
Tugdual Saunier <tugdual.saunier@gmail.com>
2015-04-29 17:04:39 +0100

    Force Blackfire repositories update on run

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -15,16 +15,25 @@ when 'rhel', 'fedora'
     gpgcheck false
     sslverify true
     sslcacert '/etc/pki/tls/certs/ca-bundle.crt'
-    action :create
+    action [:create, :makecache]
     only_if { node['blackfire']['install_repository'] }
   end
 else
-  apt_repository 'blackfire' do
+  r = apt_repository 'blackfire' do
     uri "#{node[cookbook_name]['repository']}/#{node['platform']}"
     distribution 'any'
     key 'https://packagecloud.io/gpg.key'
     components ['main']
-    action :add
+    action :nothing
     only_if { node['blackfire']['install_repository'] }
+  end
+  r.run_action(:add)
+
+  unless r.updated?
+    execute 'blackfire repository update' do
+      command "apt-get update -o Dir::Etc::sourcelist='sources.list.d/#{r.name}.list' -o Dir::Etc::sourceparts='-' -o APT::Get::List-Cleanup='0'"
+      ignore_failure true
+      action :run
+    end
   end
 end
